a6996a3f5239a39d03ba2f1cfa5d378bd896c785
vonrims <mr.mytab@gmail.com>
2026-08-27 21:57:29 +0100

    test(m0-p4): add unit tests for text analysis

--- m0-projects/p4-text-stat-calculator/src/lib.rs
+++ m0-projects/p4-text-stat-calculator/src/lib.rs
@@ -38,3 +38,57 @@ fn count_bytes(text: &str) -> usize {
 fn find_longest_word<'a>(text: &'a str) -> Option<&'a str> {
     text.split_whitespace().max_by_key(|w| w.chars().count())
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_standard_multiline_text(){
+        let input = "Rust\r\nis\r\nfast\r\n";
+        let stats = analyze(input);
+
+        assert_eq!(stats.lines, 3);
+        assert_eq!(stats.words, 3);
+        assert_eq!(stats.chars, 16);
+        assert_eq!(stats.bytes, 16);
+        assert_eq!(stats.longest_word, Some("fast"));
+    }
+
+    #[test]
+    fn test_empty_text() {
+        let input = "";
+        let stats = analyze(input);
+
+        assert_eq!(stats.lines, 0);
+        assert_eq!(stats.words, 0);
+        assert_eq!(stats.chars, 0);
+        assert_eq!(stats.bytes, 0);
+        assert_eq!(stats.longest_word, None);
+
+    }
+
+    #[test]
+    fn test_unicode_and_cyrillic() {
+        let input = "Привет Rust 🦀";
+        let stats = analyze(input);
+
+        assert_eq!(stats.lines, 1);
+        assert_eq!(stats.words, 3);
+        assert_eq!(stats.chars, 13);
+        assert_eq!(stats.bytes, 22);
+        assert_eq!(stats.longest_word, Some("Привет"));
+    }
+
+    #[test]
+    fn test_only_whitespaces() {
+        let input = "   \r\n\t  ";
+        let stats = analyze(input);
+        
+        assert_eq!(stats.lines, 2);
+        assert_eq!(stats.words, 0);
+        
+        assert_eq!(stats.longest_word, None);
+    }
+}
