e49bde58216645f9087acc99e18e8942d9d5c2c4
vonrims <mr.mytab@gmail.com>
2026-08-28 23:59:08 +0100

    feat(m0-p5): implement in-place string cleaner logic and cli

--- m0-projects/p5-string-cleaner/src/lib.rs
+++ m0-projects/p5-string-cleaner/src/lib.rs
@@ -1,17 +1,38 @@
 pub fn clean_string(input: &mut String) {
-
+    remove_control_chars(input);
+    collapse_spaces(input);
+    trim_in_place(input);
 }
 
 pub fn trim_in_place(input: &mut String) {
+    let trimmed = input.trim();
+    let start_bytes = trimmed.as_ptr() as usize - input.as_ptr() as usize;
+    let trimmed_len = trimmed.len();
 
+    input.drain(..start_bytes);
+    input.truncate(trimmed_len);
 }
 
 fn remove_control_chars(input: &mut String) {
-
+    input.retain(|ch| ch != '\t' && ch != '\r' && ch != '\n');
 }
 
 fn collapse_spaces(input: &mut String) {
+    let mut seen_space = false;
 
+    input.retain(|ch| {
+        if ch == ' '{
+            if seen_space {
+                false
+            } else {
+                seen_space = true;
+                true
+            }
+        } else {
+            seen_space = false;
+            true
+        }
+    });
 }
 
 
--- m0-projects/p5-string-cleaner/src/main.rs
+++ m0-projects/p5-string-cleaner/src/main.rs
@@ -1,9 +1,32 @@
+use std::io::{self, Write};
+
+use p5_string_cleaner::clean_string;
+
 
 
 fn main() {
+    println!("Welcome to String Cleaner!");
+    println!("Enter text to sanitize: ");
+
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = read_input();
+
+    clean_string(&mut input);
+
+    println!("\n=== CLEANED RESULT ===");
+    println!("\"{input}\"");
+    println!("======================")
+
 
 }
 
 fn read_input() -> String {
-    
+    let mut buffer = String::new();
+
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to read line from stdin");
+
+    buffer
 }
